48b8cf00aa45691cfacef6904682383692955661
ztibbitts <ztibbitts@hchb.com>
2017-11-14 11:27:24 -0500

    AutoOneToOneField things

--- fan/models.py
+++ fan/models.py
@@ -7,11 +7,29 @@ from django.urls import reverse
 
 from fanrf import settings
 
+from annoying.fields import AutoOneToOneField
+
 
 class Fan(models.Model):
     name = models.CharField(max_length=255)
     address = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(15)])
     smart = models.BooleanField()
+
+    def get_status(self):
+        s = self.state
+        return {
+            'name': self.name,
+            'brightness': s.brightness,
+            'light': s.light,
+            'fan_speed': s.fan_speed,
+        }
+    
+    def get_absolute_url(self):
+        return reverse('fan_update', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
 
 class FanState(models.Model):
     OFF = 'O'
@@ -26,7 +44,7 @@ class FanState(models.Model):
         (HIGH, 'high')
     )
 
-    fan = models.OneToOneField(Fan, related_name='state')
+    fan = AutoOneToOneField(Fan, primary_key=True, related_name='state')
     fan_speed = models.CharField(max_length=1, choices=FAN_SPEED_CHOICES, default=OFF)
     brightness = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     light = models.BooleanField(default=False)
@@ -34,19 +52,16 @@ class FanState(models.Model):
     def get_light_value(self):
         return str(self.brightness) if self.light else '0'
 
-    def __str__(self):
-        return self.name
-
     def get_command(self):
         base_command = ' '.join([settings.FANRF,
                                  '--spidev=' + settings.SPIDEV,
                                  '--irq=' + settings.IRQ,
                                  '--shutdown=' + settings.SHUTDOWN,
-                                 '--address=' + str(self.address)])
+                                 '--address=' + str(self.fan.address)])
         if self.smart:
             return ' '.join([base_command, 'smart', self.get_fan_speed_display(), self.get_light_value()])
         else:
-            return ''
+            return 'TODO'
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         cmd = self.get_command()
@@ -56,13 +71,6 @@ class FanState(models.Model):
             os.system(cmd)
         super().save(force_insert, force_update, using, update_fields)
 
-    def get_absolute_url(self):
-        return reverse('fan_update', kwargs={'pk': self.pk})
+    
 
-    def get_status(self):
-        return {
-            'name': self.name,
-            'brightness': self.brightness,
-            'light': self.light,
-            'fan_speed': self.fan_speed,
-        }
+
